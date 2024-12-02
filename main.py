a1fcd3ab2744184de722dc91f104bad190ba01a5
raz <razbe123@gmail.com>
2024-12-02 16:18:56 +0200

    plotting the middle line

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ if __name__ == '__main__':
     x_column = df.iloc[1:, 0].to_numpy()
     y_column = df.iloc[1:, 1].to_numpy()
 
+    middle_line_x = []
+    middle_line_y = []
+    for i in range(int(len(x_column)/2)):
+        middle_line_x.append((x_column[i]+x_column[i+1+int(len(x_column)/2)])/2)
+        middle_line_y.append((y_column[i]+y_column[i+1+int(len(y_column)/2)])/2)
+
     plt.figure(figsize=(10, 10))  # Optional: set figure size
     plt.scatter(x_column, y_column, marker='o')  # Scatter plot
-    #plt.scatter(middle_line_x, middle_line_y, marker='o')
+    plt.scatter(middle_line_x, middle_line_y, marker='o')
     # Add labels and title
     plt.xlabel('X Values')
     plt.ylabel('Y Values')
